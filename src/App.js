938be044aa8cfe82ada20248b5f815bf38b3dd11
Misty Bui <mishtybui@gmail.com>
2021-02-26 00:09:04 +0200

    ex2.9

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' , number: '39-44-343342' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
 
     const addContact = event => {
         event.preventDefault();
@@ -31,37 +35,52 @@ const App = () => {
 
     }
 
-const handleNameChanged = event => {
-    console.log("event.target.value");
-    setNewName(event.target.value);
-}
+    const handleNameChanged = event => {
+        console.log("event.target.value");
+        setNewName(event.target.value);
+    }
 
-const handleNumberChanged = event => {
-    console.log("event.target.value");
-    setNewNumber(event.target.value);
-}
-return (
-    <div>
-        <h2>Phonebook</h2>
-        <form onSubmit={addContact}>
-            <div>
-                name: <input value={newName} onChange={handleNameChanged} />
-            </div>
-            <div>number: <input value={newNumber} onChange={handleNumberChanged} />
-            </div>
+    const handleNumberChanged = event => {
+        console.log("event.target.value");
+        setNewNumber(event.target.value);
+    }
 
+    const handleFilter = event => {
+        console.log("event.target.value");
+        setNewFilter(event.target.value);        
+    }
+
+    const filteredList = () => 
+        newFilter.length > 0 ? persons.filter(person => person.name.toLocaleLowerCase().includes(newFilter)) : persons
+    
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
             <div>
-                <button type="submit">add</button>
+                filter shown with: <input value={newFilter} onChange={handleFilter} />
             </div>
-        </form>
-        <h2>Numbers</h2>
-        <ul>
-            {persons.map(item =>
-                <li key={persons.indexOf(item)}>{item.name} {item.number}</li>
-            )}
-        </ul>
-    </div>
-)
+            <h2>add new</h2>
+
+            <form onSubmit={addContact}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChanged} />
+                </div>
+                <div>number: <input value={newNumber} onChange={handleNumberChanged} />
+                </div>
+
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {filteredList().map(item =>
+                    <li key={filteredList().indexOf(item)}>{item.name} {item.number}</li>
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default App
